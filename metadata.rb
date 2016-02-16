42c51584595a6950713d3ea16ab1692cde53ac55
heathsnow <heath.snow@changepoint.com>
2016-02-16 13:03:50 -0800

    metadata version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Installs/Configures the 7-zip file archiver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 ver_path = File.join(File.dirname(__FILE__), 'version.txt')
-version File.exist?(ver_path) ? IO.read(ver_path).chomp : '1.0.0'
+version File.exist?(ver_path) ? IO.read(ver_path).chomp : '1.0.9'
 supports         'windows'
 
 depends          'windows', '>= 1.2.2'
