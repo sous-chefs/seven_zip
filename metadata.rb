330b93d615e7860a716953a58a3e2fb91ed4f993
heathsnow <heath.snow@changepoint.com>
2016-02-16 10:36:55 -0800

    version strategy updated

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'sneal@sneal.net'
 license          'Apache 2.0'
 description      'Installs/Configures the 7-zip file archiver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.4'
+ver_path = File.join(File.dirname(__FILE__), 'version.txt')
+version File.exist?(ver_path) ? IO.read(ver_path).chomp : '1.0.0'
 supports         'windows'
 
 depends          'windows', '>= 1.2.2'
