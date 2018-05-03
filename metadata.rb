7e1cf54ff2a3bc8ce1e3b790f2213510f729baee
Linus Ruth <linus.ruth@changepoint.com>
2018-05-03 16:25:48 -0700

    chore: fix errors identified by foodcritic

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sneal@sneal.net'
 source_url       'https://github.com/windowschefcookbooks/seven_zip'
 issues_url       'https://github.com/windowschefcookbooks/seven_zip/issues'
 chef_version     '>= 12.6' if respond_to?(:chef_version)
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures the 7-zip file archiver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.2'
