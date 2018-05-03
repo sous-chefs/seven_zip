1c63ba8d0b77998c9fe1a77844513d46c6705cc5
Linus Ruth <linus.ruth@changepoint.com>
2018-05-03 16:42:46 -0700

    chore: increment cookbook minor version in metadata

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ chef_version     '>= 12.6' if respond_to?(:chef_version)
 license          'Apache-2.0'
 description      'Installs/Configures the 7-zip file archiver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.2'
+version          '2.1.0'
 supports         'windows'
 depends          'windows'
